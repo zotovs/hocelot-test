d4ca57cc1235b11da72a0871783de3484ac25e55
Sergey Zotov <zotov.serg.web@gmail.com>
2020-01-16 15:40:53 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true
+  },
+  extends: ["react-app", "airbnb", "prettier", "prettier/react"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        trailingComma: "all",
+        jsxBracketSameLine: true,
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true
+      }
+    ],
+    "react/jsx-filename-extension": 0
+  }
+};
